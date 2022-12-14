3c1eaaaadb0c3636026cc46a71498e52fad8b299
Victor Azevedo <josevcazevedo@gmail.com>
2022-12-14 20:19:38 -0300

    feat: questao 3

--- Prática - Exercícios JOIN/03.sql
+++ Prática - Exercícios JOIN/03.sql
@@ -1,4 +1,5 @@
-SELECT u."id", u."name", c."name" AS course, s."name" AS school, e."endDate" FROM users AS u
-  JOIN schools AS s ON e."schoolId" = s."id"
-  JOIN courses AS c ON e."courseId" = c."id"
-  JOIN educations AS e ON e."userId" = u."id";
+SELECT u."id", u."name", c."name", s."name", e."endDate" FROM users AS u
+  JOIN educations AS e ON e."userId" = u."id"
+  JOIN courses AS c ON c."id" = e."courseId"
+  JOIN schools AS s ON s."id" = e."schoolId"
+  WHERE u."id" = 30;
