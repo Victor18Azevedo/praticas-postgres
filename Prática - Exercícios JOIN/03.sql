265e98b04779baa9327dbc11cf88aa83dedfb9c5
Victor Azevedo <josevcazevedo@gmail.com>
2022-12-14 20:23:14 -0300

    fix: questao 3

--- Prática - Exercícios JOIN/03.sql
+++ Prática - Exercícios JOIN/03.sql
@@ -2,4 +2,4 @@ SELECT u."id", u."name", c."name", s."name", e."endDate" FROM users AS u
   JOIN educations AS e ON e."userId" = u."id"
   JOIN courses AS c ON c."id" = e."courseId"
   JOIN schools AS s ON s."id" = e."schoolId"
-  WHERE u."id" = 30;
+  WHERE u."id" = 30 AND e."status" ILIKE 'finished';
