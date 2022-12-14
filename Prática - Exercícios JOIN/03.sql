989fb707ebca7a1df1e6e6eaf63531ec1a4e1822
Victor Azevedo <josevcazevedo@gmail.com>
2022-12-14 20:27:45 -0300

    fix: header questao 3

--- Prática - Exercícios JOIN/03.sql
+++ Prática - Exercícios JOIN/03.sql
@@ -1,4 +1,4 @@
-SELECT u."id", u."name", c."name", s."name", e."endDate" FROM users AS u
+SELECT u."id", u."name", c."name" AS course, s."name" AS school, e."endDate" FROM users AS u
   JOIN educations AS e ON e."userId" = u."id"
   JOIN courses AS c ON c."id" = e."courseId"
   JOIN schools AS s ON s."id" = e."schoolId"
